67cbec430dfc705d1a47c8384a20c3adc0e64500
Rostislav Mihnew <rostislav757@yandex.ru>
2026-02-11 22:48:22 +0300

    Add individual_3.py

---
+++ individual_3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+n = int(input("Введите натуральное число n: "))
+print(f"Натуральные делители числа {n}:")
+c = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(i, end=" ")
+        c += 1
+
+print(f"\nКоличество делителей: {c}")
